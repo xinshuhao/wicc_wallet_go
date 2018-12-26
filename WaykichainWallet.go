84ff82f0976b458dea0404f8522e161bf4fed2a1
Administrator <xinshuhao@sina.cn>
2018-12-26 21:19:40 +0800

    update

--- WaykichainWallet.go
+++ WaykichainWallet.go
@@ -1,70 +1,36 @@
 package wiccwallet
 
-import (
-	"wiccwallet/mnemonic"
-	"wiccwallet/common"
-	"encoding/hex"
-	"fmt"
-	"github.com/btcsuite/btcutil/hdkeychain"
-	"github.com/btcsuite/btcd/chaincfg"
-	"wiccwallet/bip"
-	"testing"
-)
-
-type Wallet struct {
-	Entropy     string
-	Mnemonic    string
-	Seed        string
-	MasterNode  *hdkeychain.ExtendedKey
-	PurposeNode *hdkeychain.ExtendedKey
-	Coins       []*Coin
-}
-
-type Coin struct {
-	Name    string
-	Coin    *hdkeychain.ExtendedKey
-	Network *chaincfg.Params
-}
-
-func ImportMnemonic(t *testing.T){
-	//seed := bip.NewSeed("announce parent popular hybrid fine maid exile impulse unknown school castle wage hand impulse wing", "")
-	//fmt.Println(hex.EncodeToString(seed))
-	
-	mnemonic:="announce parent popular hybrid fine maid exile impulse unknown school castle wage hand impulse wing"
-
-	entropy, _ := bip.EntropyFromMnemonic(mnemonic)
-	entropyToHexString := hex.EncodeToString(entropy)
-
-	seed, err := bip.NewSeedWithErrorChecking(mnemonic, "")
-	if err != nil {
-		fmt.Println(err)
-	}
-	seedToHexString := hex.EncodeToString(seed)
-
-	//@ToDo: create network params for FLO and LTC, etc
-
-	masterKey, _ := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
-	purposeNode, _ := masterKey.Child(0x8001869f)
-
-	wallet := Wallet{
-		Mnemonic: mnemonic,
-		Seed:     seedToHexString,
-		//ToDo: Derive entropy from mnemonic or seed
-		Entropy:     entropyToHexString,
-		MasterNode:  masterKey,
-		PurposeNode: purposeNode,
-	}
-	fmt.Println(wallet)
-}
-
+import "wiccwallet/commons"
 
 //创建助记词
 func CreateMnemonics() (string){
-   mn:= mnemonic.NewMnemonicWithLanguage(common.ENGLISH)
+   mn:= commons.NewMnemonicWithLanguage(commons.ENGLISH)
    words,err:=mn.GenerateMnemonic()
    if err!=nil{
    	return ""
    }
    return words
 }
+
+//助记词转换地址
+func Mnemonic2Address(words string,netType int)(string){
+	address := commons.GenerateAddress(words,netType)
+	return address
+}
+
+//助记词转私钥
+func Mnemonic2PrivateKey(words string,netType int)(string){
+	privateKey := commons.GeneratePrivateKey(words,netType)
+	return privateKey
+}
+
+//私钥转地址
+func PrivateKey2Address(words string,netType int)(string){
+	address := commons.ImportPrivateKey(words,netType)
+	return address
+}
+
+const WAYKI_TESTNET int = 1
+const WAYKI_MAINTNET int = 2
+
 
